96d10b624ca8036fc4f34e188be72cb08e9954c7
dmadust@gmail.com <dmadust@gmail.com>
2018-04-21 03:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  var customer = katzDeliLine.shift();
+  return `Now serving ${customer}.`;
+}
