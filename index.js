bfcbd83256bfb58e93af10977957f1f78d5a7fe0
dmadust@gmail.com <dmadust@gmail.com>
2018-04-21 03:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "The line is currently empty.";
+  }
+  
   var line = "The line is currently: ";
   for (var i = 0; i < katzDeliLine.length; i++) {
     line += `${i+1}. ${katzDeliLine[i]}, `;
